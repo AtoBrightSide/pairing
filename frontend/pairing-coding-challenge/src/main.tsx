c465b026cbdca6a24f337b9ed63bb2b3238a31b8
Abel Mesele <meseleabel99@gmail.com>
2025-04-23 16:43:26 +0300

    feature: add router to main.tsx

--- frontend/pairing-coding-challenge/src/main.tsx
+++ frontend/pairing-coding-challenge/src/main.tsx
@@ -5,14 +5,35 @@ import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { loader } from './services/attendance/attendance.service.ts'
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: 'sign-up',
+      },
+      {
+        path: 'login',
+      },
+      {
+        path: 'dashboard',
+        loader: loader,
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <App />
+        <RouterProvider router={router} />
       </Provider>
     </QueryClientProvider>
   </StrictMode>,
